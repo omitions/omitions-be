eab8a22d7a06fae6f6b27ca238a8bf882480c82f
toto.rubianto.17 <toto.rubianto.17@gmail.com>
2024-07-19 07:50:22 +0000

    add validation in register

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,28 +1,40 @@
 from flask import Blueprint, jsonify, request
 from common.database import mongo
 from app.users.services.user_register import Registration
+from bson import ObjectId
+import json
 
 users_bp = Blueprint('users', __name__)
 
 
 @users_bp.route('/register', methods=['POST'])
 def register():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    email = request.form.get('email')
+    data = request.json
+    if not data:
+        return jsonify({'message': 'Data JSON tidak ditemukan'}), 400
+
+    fullname = data.get('fullname')
+    password = data.get('password')
+    email = data.get('email')
+
+    if not fullname or not password or not email:
+        return jsonify({'message': 'Mohon masukkan fullname, password, dan email'}), 400
 
     user_data = {
-        'username': username,
+        'fullname': fullname,
         'password': password,
         'email': email
     }
-    # registration = Registration(user_data)
-    print(username, password)
-    mongo.db.users.insert_one(user_data)
+
+    result = mongo.db['users'].insert_one(user_data)
+    if result.inserted_id:
+        return jsonify({'message': 'User added successfully', 'id': str(result.inserted_id)})
+    else:
+        return jsonify({'message': 'Failed to add user'}), 500
     return jsonify(user_data)
 
 
 @users_bp.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    user = {'id': user_id, 'username': 'user{}'.format(user_id)}
+    user = {'id': user_id, 'fullname': 'user{}'.format(user_id)}
     return jsonify(user)
